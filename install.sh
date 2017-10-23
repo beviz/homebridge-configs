c0df6dba6292d0a1c33c966b5e6d374e4700f6f2
bevis <nas@bevis.me>
2017-10-23 15:06:17 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install libavahi-compat-libdnssd-dev
+npm install -g homebridge-synology
+npm install -g homebridge-mi-aqara
+npm install -g homebridge-mi-philips-light
+npm install -g homebridge-mi-ir-remote 
+npm install -g homebridge-miio
